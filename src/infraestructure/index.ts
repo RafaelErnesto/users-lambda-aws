8ce443d518265c011e4fc861d243f1e0cbc243c4
Rafael <rflernesto@gmail.com>
2021-11-25 21:32:45 -0400

    chore: change ports

--- src/infraestructure/index.ts
+++ src/infraestructure/index.ts
@@ -1,5 +1,5 @@
 import  app  from './app';
 
-app.listen(8001, async () => {
-    console.log('Listening on port 8000');
+app.listen(8080, async () => {
+    console.log('Listening on port 8080');
 });
